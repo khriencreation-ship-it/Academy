61a0a23fde2da81af071efd04924f24f6d33ed32
Deelolade <oluwanisholahabeeb13@icloud.com>
2026-02-22 11:27:56 -0800

    update: updating the home-page

--- components/home/TuitionSection.tsx
+++ components/home/TuitionSection.tsx
@@ -3,32 +3,65 @@ import Link from "next/link";
 
 import Image from "next/image";
 const TuitionSection = () => {
-    return (
-        <section className="bg-white py-12 relative ">
-            <div className="max-w-360 mx-auto h-96 relative">
-                <div className="absolute top-0 left-1/2 bg-red-400 -translate-x-1/2 -z-10 pointer-events-none">
-                    <Image
-                        src="/lines/bg-line-center.webp"
-                        alt="Background lines"
-                        width={900}
-                        height={900}
-                        className="opacity-100"
-                    />
-                </div>
-                <div className="max-w-3xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-8">
-                    <div className="flex flex-col gap-8">
-                        <Image src="/home/imageOne.jpg" alt="Quiz" width={500} height={500} className="border border-brandGreen rounded-lg" />
-                        <Image src="/home/live-class.png" alt="Live Class" width={400} height={400} className="shadow-2xl rounded-lg" />
-                    </div>
-                    <div className="flex flex-col gap-8 justify-end items-center">
-                        <Image src="/home/quiz.png" alt="Quiz" width={400} height={400} className='shadow-2xl rounded-lg' />
-                        <Image src="/home/imageTwo.jpg" alt="Quiz" width={250} height={300} className="border border-brandPurple object-cover rounded-lg" />
+  return (
+    <section className="bg-white py-12 relative overflow-hidden">
+      <div className="max-w-360 mx-auto relative z-50">
+        {/* Background lines */}
+        <div className="absolute top-0 bottom-20  left-1/2 -translate-x-1/2 z-0  pointer-events-none">
+          <Image
+            src="/lines/bg-line-center.webp"
+            className="opacity-50"
+            alt="Background lines"
+            width={1000}
+            height={1000}
+          />
+        </div>
 
-                    </div>
-                </div>
+        {/* Content */}
+        <div className="max-w-3xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-8 relative z-10">
+          {" "}
+          <div className="flex flex-col gap-10 items-end">
+            {" "}
+            <div className="relative w-75 h-75">
+              <Image
+                src="/home/imageTwo.jpg"
+                alt="Quiz"
+                fill
+                className="border border-brandGreen rounded-lg object-cover shadow-[0_0_1px_#22c55e,0_0_5px_#22c55e]"
+                />{" "}
             </div>
-        </section>
-    );
+            <div className="relative w-87.5 h-17.5 mr-12">
+              <Image
+                src="/home/live-class.png"
+                alt="Quiz"
+                fill
+                className="shadow-lg rounded-lg object-cover "
+              />
+            </div>
+          </div>{" "}
+          <div className="flex flex-col gap-10 justify-end items-start">
+            {" "}
+            <div className="relative w-87.5 h-17.5">
+              <Image
+                src="/home/quiz.png"
+                alt="Quiz"
+                fill
+                className="shadow-lg rounded-lg"
+              />
+            </div>
+            <div className="relative w-62.5 h-62.5 mx-6">
+              <Image
+                src="/home/imageOne.jpg"
+                alt="Quiz"
+                fill
+                className="object-cover border border-brandPurple rounded-lg shadow-[0_0_1px_#934ab3,0_0_5px_#934ab3]"
+              />{" "}
+            </div>
+          </div>{" "}
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default TuitionSection;
