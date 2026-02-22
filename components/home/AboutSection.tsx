ca8aa577d3e814da5433bca4ae63fcbd5dc4a2f7
Deelolade <oluwanisholahabeeb13@icloud.com>
2026-02-22 11:43:18 -0800

    update: updating the about section-1

--- components/home/AboutSection.tsx
+++ components/home/AboutSection.tsx
@@ -6,12 +6,12 @@ const AboutSection = () => {
         <section className="max-w-360 py-12 md:py-16 lg:py-20 px-4 md:px-8 lg:px-12 mx-auto bg-black my-6 z-20">
             <div className="grid md:grid-cols-2 gap-8 md:gap-12 items-center">
                 <div className="text-left">
-                    <h2 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl max-w-3xl font-normal mb-6 md:mb-8 text-white tracking-tight uppercase italic leading-tight">
-                        <span className="text-brandPurple">Education</span> Built With <span className="text-brandPurple">Purpose</span>
+                    <h2 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl max-w-md font-normal mb-6 md:mb-8 text-white tracking-tight leading-tight">
+                        Education Built With Purpose.
                     </h2>
                     
                 </div>
-                <div className="text-base md:text-lg lg:text-xl text-gray-600 leading-relaxed">
+                <div className="text-base md:text-lg lg:text-xl text-white leading-relaxed">
                         <p>
                             We focus on clarity over complexity, structure over chaos, and learning that leads to real understanding.
                         </p>
