93ebd708b7fbcff2d4db9f546466f01b00ca318b
Deelolade <oluwanisholahabeeb13@icloud.com>
2026-02-23 01:43:08 +0100

    update: integrated the whatsapp logic

--- app/page.tsx
+++ app/page.tsx
@@ -5,10 +5,12 @@ import TuitionSection from '@/components/home/TuitionSection'
 import LearningApproachSection from '@/components/home/LearningApproachSection'
 import FAQSection from '@/components/home/FAQSection'
 import Introvideo from '@/components/home/Introvideo-section'
+import WhatsApp from '@/components/Whatsapp'
 
 const page = () => {
   return (
     <main>
+      <WhatsApp />
       <Hero />
       <AboutSection />
       <Introvideo />
