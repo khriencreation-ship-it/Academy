e80271b0c496c86f2eea2bb6ffe87b6ae9f863e9
Deelolade <oluwanisholahabeeb13@icloud.com>
2026-03-01 05:26:29 +0100

    updated deployed version

--- app/apply/page.tsx
+++ app/apply/page.tsx
@@ -1,9 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
-import Link from "next/link";
 import Form from "@/components/apply/Form";
-import DarkVeil from "@/components/DarkVeil";
 import { ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 
